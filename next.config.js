4f3a3d433e0b2db1c16065ac0ef59e87e4059717
Franz <franz.lutzenberger@gmail.com>
2021-11-06 13:57:57 +0100

    add basepath and assetprefix

--- next.config.js
+++ next.config.js
@@ -4,4 +4,6 @@ const {withKeystone} = require('@keystone-next/keystone/next')
 
 module.exports = withKeystone({
   reactStrictMode: true,
+  basePath: '/next-keystone-blog',
+  assetPrefix: '/next-keystone-blog'
 })
